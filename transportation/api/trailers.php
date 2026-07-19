361a99e45825fbdc61820c9a96b517cdf9b3fa5c
horoma2911 <allyhoroma424@gmail.com>
2026-07-19 17:52:03 +0300

    Delete transportation directory

--- transportation/api/trailers.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/bootstrap.php';
-
-crud_handler('trailers', [
-    'type'     => 'as_string',
-    'capacity' => 'as_string_or_null',
-    'plate'    => 'as_string_or_null',
-    'status'   => 'as_string_or_null',
-]);
