1ebaf775b432a482b99c615fff0d9062bb4790c9
horoma2911 <allyhoroma424@gmail.com>
2026-07-24 19:19:40 +0300

    Update schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -141,7 +141,7 @@ CREATE TABLE password_resets (
 
 CREATE TABLE activities (
     id          SERIAL PRIMARY KEY,
-    usename        VARCHAR(150),
+    username        VARCHAR(150),
     action      VARCHAR(255),
     time        VARCHAR(100),
     icon        VARCHAR(50)
