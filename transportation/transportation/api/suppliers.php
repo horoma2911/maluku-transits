ebfbeff1e0718798b771bc96f82b3e7439e8fde1
horoma <allyhoroma424@gmail.com>
2026-07-24 17:48:43 +0300

    Initial commit

--- transportation/transportation/api/suppliers.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/bootstrap.php';
-
-crud_handler('suppliers', [
-    'name'     => 'as_string',
-    'category' => 'as_string_or_null',
-    'email'    => 'as_string_or_null',
-    'status'   => 'as_string_or_null',
-]);
